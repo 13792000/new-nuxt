e3705e09abd8dabaa5ee94bc9094986f5f7c8193
hosein <hosein.asgari2014@gmail.com>
2020-12-12 18:28:31 +0330

    sadf

---
+++ pages/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container>
+    <div class="d-flex justify-content-center flex-column px-0 px-sm-3">
+        <div class="mb-2 ">
+          <label>email</label>
+          <b-form-input></b-form-input>
+        </div>
+        <div>
+          <label>password</label>
+          <b-form-input></b-form-input>
+        </div>
+        <div>
+          <b-button>login</b-button>
+        </div>
+    </div>
+  </b-container>
+</template>
